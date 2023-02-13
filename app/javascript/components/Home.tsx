32205505604e44cda2972863d76ab0b8441846e3
Renny Ren <rennyallen@hotmail.com>
2023-02-13 14:38:48 +0800

    initial commit

---
+++ app/javascript/components/Home.tsx
@@ -0,0 +1,4 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+export default () => <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center"></div>
---
+++ app/javascript/components/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App"
+
+document.addEventListener("turbo:load", () => {
+  const root = createRoot(document.body.appendChild(document.createElement("div")))
+  root.render(<App />)
+})
