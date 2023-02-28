086fb475018c1ed140fa250944e852cc0c3ec368
Renny Ren <rennyallen@hotmail.com>
2023-02-28 16:46:37 +0800

    tweak: sidebar

--- app/javascript/components/MobileMenu.tsx
+++ app/javascript/components/MobileMenu.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import Sidebar from "./Sidebar"
+import Menu from "./Menu"
 
 interface MobileMenuProps {
   setIsShow: boolean
@@ -67,87 +68,7 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ setIsShow, onShowSignInModal })
                 </div>
               </div>
               <div className="fixed left-0 top-14 bottom-0 w-full overflow-y-auto bg-white px-4 pt-6 pb-4 shadow-lg shadow-zinc-900/10 ring-1 ring-zinc-900/7.5 dark:bg-zinc-900 dark:ring-zinc-800 min-[416px]:max-w-sm sm:px-6 sm:pb-10 translate-x-0">
-                <nav>
-                  <ul role="list">
-                    <li className="relative mt-6 md:mt-0">
-                      <h2 className="text-xs font-semibold text-zinc-900 dark:text-white">会话</h2>
-                      <div className="relative mt-3 pl-2">
-                        <div
-                          className="absolute inset-x-0 top-0 bg-zinc-800/2.5 will-change-transform dark:bg-white/2.5"
-                          style={{ height: "64px", top: "32px", borderRadius: "8px", opacity: "1" }}
-                        ></div>
-                        <div className="absolute inset-y-0 left-2 w-px bg-zinc-900/10 dark:bg-white/5"></div>
-                        <div className="absolute left-2 h-6 w-px bg-emerald-500" style={{ top: "36px", opacity: 1 }}></div>
-                        <ul role="list" className="border-l border-transparent">
-                          <li className="relative">
-                            <a
-                              className="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-                              href="#"
-                            >
-                              <span className="truncate">会话1</span>
-                            </a>
-                          </li>
-                          <li className="relative">
-                            <a
-                              className="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-                              href="#"
-                            >
-                              <span className="truncate">会话2</span>
-                            </a>
-                          </li>
-                        </ul>
-                      </div>
-                    </li>
-                    <li className="relative mt-6">
-                      <h2 className="text-xs font-semibold text-zinc-900 dark:text-white">菜单</h2>
-                      <div className="relative mt-3 pl-2">
-                        <div className="absolute inset-y-0 left-2 w-px bg-zinc-900/10 dark:bg-white/5"></div>
-                        <ul role="list" className="border-l border-transparent">
-                          <li className="relative">
-                            <a
-                              className="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-                              href="#"
-                            >
-                              <span className="truncate">清空会话</span>
-                            </a>
-                          </li>
-                          <li className="relative">
-                            <a
-                              className="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-                              href="/conversations"
-                            >
-                              <span className="truncate">夜间模式</span>
-                            </a>
-                          </li>
-                          <li className="relative">
-                            <a
-                              className="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-                              href="#"
-                            >
-                              <span className="truncate">常见问题</span>
-                            </a>
-                          </li>
-                          <li className="relative">
-                            <a
-                              className="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-                              href="/disclaimer"
-                            >
-                              <span className="truncate">免责声明</span>
-                            </a>
-                          </li>
-                        </ul>
-                      </div>
-                    </li>
-                    <li className="sticky bottom-0 z-10 mt-6 min-[416px]:hidden">
-                      <div
-                        className="inline-flex gap-0.5 justify-center overflow-hidden text-sm font-medium transition rounded-full bg-zinc-900 py-1 px-3 text-white hover:bg-zinc-700 dark:bg-emerald-500 dark:text-white dark:hover:bg-emerald-400 w-full"
-                        onClick={onShowSignInModal}
-                      >
-                        登录
-                      </div>
-                    </li>
-                  </ul>
-                </nav>
+                <Menu isMobile={true} onShowSignInModal={onShowSignInModal} />
               </div>
             </div>
           </div>
--- app/javascript/components/Sidebar.tsx
+++ app/javascript/components/Sidebar.tsx
@@ -1,89 +1,12 @@
 import React, { useState } from "react"
+import Menu from "./Menu"
 
 interface SidebarProps {}
 
 const Sidebar: React.FC<SidebarProps> = ({}) => {
   return (
     <nav className="hidden lg:mt-10 lg:block">
-      <ul role="list">
-        <li className="relative mt-6 md:mt-0">
-          <h2 className="text-xs font-semibold text-zinc-900 dark:text-white">会话</h2>
-          <div className="relative mt-3 pl-2">
-            <div
-              className="absolute inset-x-0 top-0 bg-zinc-800/2.5 will-change-transform dark:bg-white/2.5"
-              style={{
-                height: "32px",
-                top: "0px",
-                opacity: "1",
-                borderRadius: "2.95203% / 25%",
-                transform: "none",
-                transformOrigin: "50% 50% 0px",
-              }}
-            ></div>
-            <div className="absolute inset-y-0 left-2 w-px bg-zinc-900/10 dark:bg-white/5"></div>
-            <div className="absolute left-2 h-6 w-px bg-emerald-500" style={{ top: "4px", opacity: 1 }}></div>
-            <ul role="list" className="border-l border-transparent">
-              <li className="relative">
-                <a
-                  aria-current="page"
-                  className="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-900 dark:text-white"
-                  href="/"
-                >
-                  <span className="truncate">会话1</span>
-                </a>
-              </li>
-              <li className="relative">
-                <a
-                  className="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-                  href="/"
-                >
-                  <span className="truncate">会话2</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </li>
-        <li className="relative mt-6">
-          <h2 className="text-xs font-semibold text-zinc-900 dark:text-white">菜单</h2>
-          <div className="relative mt-3 pl-2">
-            <div className="absolute inset-y-0 left-2 w-px bg-zinc-900/10 dark:bg-white/5"></div>
-            <ul role="list" className="border-l border-transparent">
-              <li className="relative">
-                <a
-                  className="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-                  href="/contacts"
-                >
-                  <span className="truncate">清空会话</span>
-                </a>
-              </li>
-              <li className="relative">
-                <a
-                  className="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-                  href="/conversations"
-                >
-                  <span className="truncate">夜间模式</span>
-                </a>
-              </li>
-              <li className="relative">
-                <a
-                  className="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-                  href="/messages"
-                >
-                  <span className="truncate">常见问题</span>
-                </a>
-              </li>
-              <li className="relative">
-                <a
-                  className="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-                  href="/disclaimer"
-                >
-                  <span className="truncate">免责声明</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </li>
-      </ul>
+      <Menu />
     </nav>
   )
 }
