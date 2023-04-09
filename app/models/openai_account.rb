e6488f151d005867855fd69c4d5e5ecdeb7916b6
Renny Ren <rennyallen@hotmail.com>
2023-04-09 23:39:18 +0800

    tweak: validate secret_key

--- app/models/openai_account.rb
+++ app/models/openai_account.rb
@@ -2,4 +2,6 @@ class OpenaiAccount < ApplicationRecord
   belongs_to :user, optional: true
 
   scope :engaged, -> { where.not(user_id: nil) }
+
+  validates :secret_key, presence: true, uniqueness: true
 end
