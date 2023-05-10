3b99947de56661c614befb8ad2ed73712e3947b3
Renny Ren <rennyallen@hotmail.com>
2023-05-10 11:48:23 +0800

    style: collapse header

--- app/javascript/components/CustomModel/Header.tsx
+++ app/javascript/components/CustomModel/Header.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import ModelActions from "./ModelActions"
+import { UpOutlined, DownOutlined } from "@ant-design/icons"
 
 interface HeaderProps {
   model: any
@@ -9,25 +10,50 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ model, setModel, isAddContext, handleContextChange }) => {
+  const [isOpen, setIsOpen] = useState(true)
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <>
-      <div className="py-1 md:py-3 border-b border-gray-300 border-dashed">
-        <div className="px-4 sm:px-0">
-          <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-600">{model.description}</p>
-          <div className="flex justify-between items-end">
-            <div className="mt-1 max-w-2xl text-xs text-gray-600 flex items-center">
-              <span>创建者：</span>
-              <img className="inline-block rounded-full mx-1 h-5 w-5" src={model.user_avatar_url} />
-              <span>{model.user_nickname}</span>
-            </div>
+      <div className="group">
+        <div
+          className={`border-gray-300 border-dashed overflow-hidden transition-all duration-300 ${
+            isOpen ? "max-h-36 border-b pb-2" : "max-h-0"
+          }`}
+        >
+          <div className="px-4 sm:px-0">
+            <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-600">{model.description}</p>
 
-            <ModelActions
-              model={model}
-              setModel={setModel}
-              isAddContext={isAddContext}
-              handleContextChange={handleContextChange}
-            />
+            <div className="flex justify-between items-end">
+              <div className="mt-1 max-w-2xl text-xs text-gray-600 flex items-center">
+                <span>创建者：</span>
+                <img className="inline-block rounded-full mx-1 h-5 w-5" src={model.user_avatar_url} />
+                <span>{model.user_nickname}</span>
+              </div>
+
+              <ModelActions
+                model={model}
+                setModel={setModel}
+                isAddContext={isAddContext}
+                handleContextChange={handleContextChange}
+              />
+            </div>
           </div>
+        </div>
+
+        <div className="flex justify-center">
+          {isOpen ? (
+            <button className="outline-none fixed -mt-6 opacity-0 group-hover:opacity-100 z-50" onClick={toggleMenu}>
+              <UpOutlined className="text-gray-400" />
+            </button>
+          ) : (
+            <button className="outline-none fixed -mt-3 z-20 group-hover:-mt-2 w-[50%]" onClick={toggleMenu}>
+              <DownOutlined className="text-gray-300" />
+            </button>
+          )}
         </div>
       </div>
     </>
--- app/javascript/components/CustomModel/index.tsx
+++ app/javascript/components/CustomModel/index.tsx
@@ -85,15 +85,9 @@ const CustomModel: React.FC<CustomModelProps> = ({ conversation }) => {
   return (
     <>
       <Helmet>
-        <title>{conversation?.title || state?.conversationTitle || `${model.title} - aii.chat`}</title>
+        <title>{conversation?.title || state?.conversationTitle || `${model.title || ""} - aii.chat`}</title>
       </Helmet>
       <div className="h-full relative pt-12 md:pt-14">
-        {isPrivate && (
-          <div className="flex items-center justify-center h-full">
-            <Empty description="该模型为私有模型，仅作者可访问" />
-          </div>
-        )}
-
         <main className="h-full">
           <Background />
           {isLoading ? (
@@ -102,39 +96,47 @@ const CustomModel: React.FC<CustomModelProps> = ({ conversation }) => {
             </div>
           ) : (
             <>
-              <div className="relative h-full w-full transition-width flex flex-col overflow-y-auto items-stretch flex-1 pb-20">
-                <div className="flex-1 overflow-hidden relative">
-                  <div className="h-full overflow-hidden">
-                    <div className="flex h-full antialiased text-gray-800">
-                      <div className="flex flex-row h-full w-full overflow-x-hidden">
-                        <div className="flex flex-col flex-auto h-full items-center">
-                          <div className="flex flex-col flex-auto flex-shrink-0 rounded-2xl h-full w-full md:max-w-3xl lg:max-w-4xl">
-                            <div className="flex flex-col h-full md:pb-4">
-                              <div className="flex flex-col h-full overflow-x-auto">
-                                {!!Object.keys(model).length ? (
-                                  <>
-                                    <Header
-                                      model={model}
-                                      setModel={setModel}
-                                      isAddContext={isAddContext}
-                                      handleContextChange={handleContextChange}
-                                    />
-                                    <MessageList
-                                      gptName={model.title}
-                                      messages={messages}
-                                      isLoading={isLoading}
-                                      avatarUrl={model.avatar_url}
-                                      voice={model.voice}
-                                    />
-                                  </>
-                                ) : (
-                                  <MessageList
-                                    gptName="AI"
-                                    messages={messages}
-                                    isLoading={isLoading}
-                                    avatarUrl={`${CDN_HOST}/assets/person.png`}
-                                  />
-                                )}
+              {isPrivate ? (
+                <div className="flex items-center justify-center h-full">
+                  <Empty description="该模型为私有模型，仅作者可访问" />
+                </div>
+              ) : (
+                <>
+                  <div className="relative h-full w-full transition-width flex flex-col overflow-y-auto items-stretch flex-1 pb-20">
+                    <div className="flex-1 overflow-hidden relative">
+                      <div className="h-full overflow-hidden">
+                        <div className="flex h-full antialiased text-gray-800">
+                          <div className="flex flex-row h-full w-full overflow-x-hidden">
+                            <div className="flex flex-col flex-auto h-full items-center">
+                              <div className="flex flex-col flex-auto flex-shrink-0 rounded-2xl h-full w-full md:max-w-3xl lg:max-w-4xl">
+                                <div className="flex flex-col h-full md:pb-4">
+                                  <div className="flex flex-col h-full overflow-x-auto">
+                                    {!!Object.keys(model).length ? (
+                                      <>
+                                        <Header
+                                          model={model}
+                                          setModel={setModel}
+                                          isAddContext={isAddContext}
+                                          handleContextChange={handleContextChange}
+                                        />
+                                        <MessageList
+                                          gptName={model.title}
+                                          messages={messages}
+                                          isLoading={isLoading}
+                                          avatarUrl={model.avatar_url}
+                                          voice={model.voice}
+                                        />
+                                      </>
+                                    ) : (
+                                      <MessageList
+                                        gptName="AI"
+                                        messages={messages}
+                                        isLoading={isLoading}
+                                        avatarUrl={`${CDN_HOST}/assets/person.png`}
+                                      />
+                                    )}
+                                  </div>
+                                </div>
                               </div>
                             </div>
                           </div>
@@ -142,31 +144,31 @@ const CustomModel: React.FC<CustomModelProps> = ({ conversation }) => {
                       </div>
                     </div>
                   </div>
-                </div>
-              </div>
 
-              {!!Object.keys(model).length ? (
-                <Footer
-                  isLoading={isLoading}
-                  setIsLoading={setIsLoading}
-                  messages={messages}
-                  setMessages={setMessages}
-                  conversationType="custom"
-                  modelId={model.id}
-                  placeholder={model.input_placeholder}
-                  isAddContext={isAddContext}
-                />
-              ) : (
-                <div className="absolute bottom-0 left-0 w-full dark:border-transparent bg-vert-light-gradient dark:bg-vert-dark-gradient input-area">
-                  <form className="stretch mx-2 flex flex-row items-center gap-1 md:gap-2 last:mb-2 md:last:mb-6 lg:mx-auto lg:max-w-3xl">
-                    <div className="cursor-pointer flex flex-col w-full py-2 flex-grow md:pl-2 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-sm">
-                      <div className="flex h-6 w-full items-center pl-2 pr-3 text-sm text-zinc-500 transition dark:text-zinc-400 focus:[&amp;:not(:focus-visible)]:outline-none">
-                        <MinusCircleOutlined />
-                        <span className="pl-1">由于该对话模型已被作者删除，无法继续对话</span>
-                      </div>
+                  {!!Object.keys(model).length ? (
+                    <Footer
+                      isLoading={isLoading}
+                      setIsLoading={setIsLoading}
+                      messages={messages}
+                      setMessages={setMessages}
+                      conversationType="custom"
+                      modelId={model.id}
+                      placeholder={model.input_placeholder}
+                      isAddContext={isAddContext}
+                    />
+                  ) : (
+                    <div className="absolute bottom-0 left-0 w-full dark:border-transparent bg-vert-light-gradient dark:bg-vert-dark-gradient input-area">
+                      <form className="stretch mx-2 flex flex-row items-center gap-1 md:gap-2 last:mb-2 md:last:mb-6 lg:mx-auto lg:max-w-3xl">
+                        <div className="cursor-pointer flex flex-col w-full py-2 flex-grow md:pl-2 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-sm">
+                          <div className="flex h-6 w-full items-center pl-2 pr-3 text-sm text-zinc-500 transition dark:text-zinc-400 focus:[&amp;:not(:focus-visible)]:outline-none">
+                            <MinusCircleOutlined />
+                            <span className="pl-1">由于该对话模型已被作者删除，无法继续对话</span>
+                          </div>
+                        </div>
+                      </form>
                     </div>
-                  </form>
-                </div>
+                  )}
+                </>
               )}
             </>
           )}
