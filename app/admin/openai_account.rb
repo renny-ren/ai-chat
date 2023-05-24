4957e699256ab08c23edc194a8c2be8ea4e052ec
Renny Ren <rennyallen@hotmail.com>
2023-05-25 01:08:58 +0800

    tweak:  admin account form

--- app/admin/openai_account.rb
+++ app/admin/openai_account.rb
@@ -20,6 +20,10 @@ ActiveAdmin.register OpenaiAccount do
   form do |f|
     f.inputs do
       f.input :user_id
+      f.input :secret_key
+      f.input :credit
+      f.input :used_amount
+      f.input :total_available
     end
     f.actions
   end
