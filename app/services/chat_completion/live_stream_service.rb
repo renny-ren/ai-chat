768cda7fc806155974390403c625512274adc76a
Renny Ren <rennyallen@hotmail.com>
2023-06-14 16:27:49 +0800

    tweak: use model gpt-3.5-turbo-0613

--- app/services/chat_completion/live_stream_service.rb
+++ app/services/chat_completion/live_stream_service.rb
@@ -1,6 +1,6 @@
 module ChatCompletion
   class LiveStreamService
-    MODEL = "gpt-3.5-turbo".freeze
+    MODEL = "gpt-3.5-turbo-0613".freeze
 
     attr_reader :sse, :current_user, :params
     attr_accessor :result
