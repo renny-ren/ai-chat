c4faf651de716e91090cefb64a0b7082a27c7690
Renny Ren <rennyallen@hotmail.com>
2023-06-15 17:44:43 +0800

    fix: user avatar url

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ class User < ApplicationRecord
   def avatar_url(size = 80)
     # ActiveStorage::Current.url_options = { host: "localhost", port: 3000 }
     Rails.cache.fetch("user_#{id}_avatar_url", expires_in: 2.hours) do
-      avatar.url || "user_avatar/?name=#{nickname}&size=#{size}"
+      avatar.url || "/user_avatar/?name=#{nickname}&size=#{size}"
     end
   end
 
